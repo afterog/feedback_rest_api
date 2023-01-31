84c7d3269234beb4acaa5eac6445fb713390f725
afteralexxo <itsalextwod@gmail.com>
2023-01-30 19:40:43 -0800

    trying

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ let allowCrossDomain = function (req, res, next) {
     next();
 }
 
-app.use(cors());
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }))
-app.use(allowCrossDomain);
+app.use(allowCrossDomain)
 
 
 
