fc0148fadcc2cc228debf04d193ad56ab27e65c4
afteralexxo <itsalextwod@gmail.com>
2023-01-30 19:35:31 -0800

    add cors

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const app = express()
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config()
+
+
+
+let allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}))
+app.use(allowCrossDomain);
+
 
 
 const uri = process.env.DATABASE;
@@ -12,28 +30,9 @@ async function main() {
   await mongoose.connect(uri, { useNewUrlParser: true, bufferCommands: false });
 }
 
-main();
-
-
-let allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-}
-
-app.use(express.json());
-
-app.use(express.urlencoded({
-    extended: false
-}))
-
-app.use(allowCrossDomain);
-
-
-
-app.get('/api', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
+    main();
     User.find({}, (err, user) => {
       res.json(user)
     });
