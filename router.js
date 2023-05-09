b68677e477c594bb4551826ab662c1d5f6de7305
afteralexxo <itsalextwod@gmail.com>
2023-05-08 21:31:43 -0700

    upgrade the cv

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const route = express.Router();
 const fs = require('fs');
 const path = require('path');
-const { findData, insertData }= require('./crud');
+const { findData, insertData } = require('./crud');
 
 // Middleware function to log incoming requests
 const requestLogger = (req, res, next) => {
@@ -15,39 +15,39 @@ route.use(requestLogger);
 
 // Define routes
 route.post("/setdata", async (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const feedback = req.body.feedback;
-    const obj = {fullName: name, email: email, feedback: feedback};
-    const data = await findData({fullName: name})
+  const name = req.body.name;
+  const email = req.body.email;
+  const feedback = req.body.feedback;
+  const obj = { fullName: name, email: email, feedback: feedback };
+  const data = await findData({ fullName: name })
 
 
-    if ( await insertData(obj)) {
-      res.status(200).send(true)
-    }
-    else{
-      res.send(false)
-    }
-   
-  });
-  
+  if (await insertData(obj)) {
+    res.status(200).send(true)
+  }
+  else {
+    res.send(false)
+  }
+
+});
+
 route.get('/getdata', async (req, res) => {
-    const data = await findData({})
-    console.log('send data from database to client');
-    res.send(data)
-  })
-  
+  const data = await findData({})
+  console.log('send data from database to client');
+  res.send(data)
+})
+
 route.get('/pdf', (req, res) => {
   let url = __dirname
   url = url.replace(/\\routes$/, "");
-  url = path.join(url, 'Alemayehu_mekonen[cv].pdf')
-  
-    // Set the content type to application/pdf to force the browser to download the file
-    res.type('application/pdf');
-    res.setHeader('Content-Disposition', 'attachment; filename="Alemayehu[CV].pdf"');
-    // Read the file from disk and pipe it to the response object
-    const stream = fs.createReadStream(url);
-    stream.pipe(res);
-  });
-  
+  url = path.join(url, 'Alemayehu[CV].pdf')
+
+  // Set the content type to application/pdf to force the browser to download the file
+  res.type('application/pdf');
+  res.setHeader('Content-Disposition', 'attachment; filename="Alemayehu[CV].pdf"');
+  // Read the file from disk and pipe it to the response object
+  const stream = fs.createReadStream(url);
+  stream.pipe(res);
+});
+
 module.exports = route;
