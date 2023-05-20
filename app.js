02e910331914920cdc045664327a57376eeb643b
afteralexxo <itsalextwod@gmail.com>
2023-05-20 06:52:25 -0700

    fix start script

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//  includes the required modules including express, mongoose, cors and dotenv.
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -7,7 +6,6 @@ const mongoose = require('mongoose');
 const route = require('./router');
 require('dotenv').config();
 
-// allowCrossDomain sets up CORS headers to allow cross-domain requests to the API.
 let allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -15,19 +13,15 @@ let allowCrossDomain = function (req, res, next) {
   next();
 }
 
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// set up express middleware including cors, parsing of JSON and URL-encoded requests, and the allowCrossDomain function.
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({
-  extended: false
-}))
-app.use(allowCrossDomain)
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(allowCrossDomain);
 
 app.use('/', route);
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 try {
   mongoose.set('strictQuery', true);
   mongoose
@@ -45,7 +39,6 @@ try {
     .catch(error => {
       console.error(error.message);
     });
-
 } catch (error) {
   console.log(error.message);
 }
